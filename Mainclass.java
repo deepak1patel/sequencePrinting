ce289d6a3bcd4299757f2cd6016cbe9be3fc4e27
deepak1patel <deepakpatelg16@gmail.com>
2019-08-03 07:33:42 +0530

    making over layout proper

--- Mainclass.java
+++ Mainclass.java
@@ -66,6 +66,9 @@ class PattrenMatrix{
   static void matrixPrinting(){
     for(int i = 0;i < 4; i++){
       for(int j = 0;j < 4;j++){
+        if(mat[i][j]<=9){
+          System.out.print(" ");
+        }
         System.out.print(mat[i][j]+" ");
       }
       System.out.println();
